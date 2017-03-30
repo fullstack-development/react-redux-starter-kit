95af24f9c45de872c18b508b00568ec997032eac
Dmitriy Serdtsev <in19farkt@gmail.com>
2017-03-30 11:51:41 +0700

    setting scssLoader in prod.config.js

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -11,129 +11,128 @@ const stylelint = require('stylelint');
 const doiuse = require('doiuse');
 
 const prodConfig = Object.assign({}, devConfig);
-const stylLoader = {
-    test: /\.styl$/,
-    loader: ExtractTextPlugin.extract({
-        loader: [
-            'style-loader',
-            'css-loader?importLoaders=1',
-            {
-                loader: 'postcss-loader',
-                options: {
-                    plugins: function () {
-                        return [
-                            autoprefixer({browsers: ['last 2 versions']}),
-                        ];
-                    },
-                },
-            },
-            'sass-loader',
-            {
-                loader: 'postcss-loader',
-                options: {
-                    syntax: postcssSCSS,
-                    plugins: function () {
-                        return [
-                            postcssEasyImport({
-                                extensions: '.scss',
-                            }),
-                            stylelint(),
-                            doiuse({
-                                browsers:['ie >= 11', 'last 2 versions'],
-                                ignore: ['flexbox', 'rem'],
-                                ignoreFiles: ['**/normalize.css'],
-                            }),
-                            postcssReporter({
-                                clearReportedMessages: true,
-                                throwError: true,
-                            }),
-                        ];
-                    },
-                },
-            },
-        ],
-    })
+const scssLoader = {
+  test: /\.scss$/,
+  use: ExtractTextPlugin.extract({
+    use: [
+      'css-loader?importLoaders=1',
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: function () {
+            return [
+              autoprefixer({browsers: ['last 2 versions']}),
+            ];
+          },
+        },
+      },
+      'sass-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          syntax: postcssSCSS,
+          plugins: function () {
+            return [
+              postcssEasyImport({
+                extensions: '.scss',
+              }),
+              stylelint(),
+              doiuse({
+                browsers: ['ie >= 11', 'last 2 versions'],
+                ignore: ['flexbox', 'rem'],
+                ignoreFiles: ['**/normalize.css'],
+              }),
+              postcssReporter({
+                clearReportedMessages: true,
+                throwError: true,
+              }),
+            ];
+          },
+        },
+      },
+    ],
+  }),
 };
 
 const cssLoader = {
-    test: /\.css$/,
-    loader:  ExtractTextPlugin.extract({
-        loader: 'css-loader',
-        fallbackLoader: 'style-loader',
-    })
+  test: /\.css$/,
+  use: ExtractTextPlugin.extract({
+    use: 'css-loader',
+  })
 };
 
 const scriptsLoader = {
-    test: /\.(ts|tsx)$/,
-    use: [
-        {
-            loader: 'babel-loader',
-            options: { presets: ['es2015'], plugins: ["transform-regenerator"] }
-        },
-        {
-            loader: 'ts-loader',
-            options: { logLevel: 'debug' }
-        },
-        'tslint-loader'
-    ],
+  test: /\.(ts|tsx)$/,
+  use: [
+    {
+      loader: 'babel-loader',
+      options: { presets: ['es2015'], plugins: ["transform-regenerator"] }
+    },
+    {
+      loader: 'ts-loader',
+      options: { logLevel: 'debug' }
+    },
+    'tslint-loader'
+  ],
 };
 
 // replace modules, if already exists modules with same patterns
 prodConfig.module.rules = prodConfig.module.rules.map(loader => {
-    switch (loader.test.toString()) {
-        case stylLoader.test.toString():
-            return stylLoader;
-        case cssLoader.test.toString():
-            return cssLoader;
-        case scriptsLoader.test.toString():
-            return scriptsLoader;
-        default: return loader;
-    }
+  switch (loader.test.toString()) {
+    case scssLoader.test.toString():
+      return scssLoader;
+    case cssLoader.test.toString():
+      return cssLoader;
+    case scriptsLoader.test.toString():
+      return scriptsLoader;
+    default:
+      return loader;
+  }
 });
 
 prodConfig.plugins = [
-    new ExtractTextPlugin({
-        filename: 'css/[name].css',
-        allChunks: true,
-    }),
-    new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'assets/index.html'
-    }),
-    new webpack.DefinePlugin({
-        "process.env": {
-            NODE_ENV: JSON.stringify('production'),
-            __HOST__: JSON.stringify('http://localhost:3000'),
-        }
-    }),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false,
-            screw_ie8: true,
-            conditionals: true,
-            unused: true,
-            comparisons: true,
-            sequences: true,
-            dead_code: true,
-            evaluate: true,
-            if_return: true,
-            join_vars: true,
-        },
-        output: {
-            comments: false
-        },
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        filename: 'js/vendor.bundle.js',
-        minChunks: Infinity,
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-        name: 'shared',
-        filename: 'js/shared.bundle.js',
-        chunks: ['app']
-    }),
+  new ExtractTextPlugin({
+    filename: 'css/[name].css',
+    allChunks: true,
+  }),
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'assets/index.html'
+  }),
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: JSON.stringify('production'),
+      __HOST__: JSON.stringify('http://localhost:3000'),
+    }
+  }),
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+      conditionals: true,
+      unused: true,
+      comparisons: true,
+      sequences: true,
+      dead_code: true,
+      evaluate: true,
+      if_return: true,
+      join_vars: true,
+    },
+    output: {
+      comments: false
+    },
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'js/vendor.bundle.js',
+    minChunks: Infinity,
+  }),
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'shared',
+    filename: 'js/shared.bundle.js',
+    chunks: ['app']
+  }),
 ];
 
 
