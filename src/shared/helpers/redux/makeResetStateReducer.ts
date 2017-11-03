1513cfc0ec30f25feee125c47b9fc85dca193fba
Gulyaev Kirill <gulyaev52@gmail.com>
2017-11-03 18:34:45 +0700

    Improved makeResetStateReducer.

--- src/shared/helpers/redux/makeResetStateReducer.ts
+++ src/shared/helpers/redux/makeResetStateReducer.ts
@@ -1,5 +1,7 @@
 import { IAction } from './namespace';
 
-export default function makeResetStateReducer<A extends IAction, S>(type: A['type'], initialState: S) {
-  return (state: S, action: A) => action.type === type ? initialState : state;
+export default function makeResetStateReducer<A extends IAction>(actionType: A['type']) {
+  return <S>(initialState: S) => {
+    return (state: S, action: A) => action.type === actionType ? initialState : state;
+  };
 }
