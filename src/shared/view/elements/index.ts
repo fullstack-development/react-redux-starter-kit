b0073125d2ce8ef56fafacd12c4c644565c2e683
clicktronix <s28.morgan@gmail.com>
2018-12-25 17:46:06 +0600

    Deleted extra elemets

--- src/shared/view/elements/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as RowsLayout } from './RowsLayout/RowsLayout';
-export { default as SimpleList } from './SimpleList/SimpleList';
-export { default as SelectInput, Option } from './SelectInput/SelectInput';
-export { default as TextInput } from './TextInput/TextInput';
