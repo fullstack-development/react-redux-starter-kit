7e5b9aeff28d19315fc811e40670159a463b8dda
clicktronix <s28.morgan@gmail.com>
2019-02-09 15:36:59 +0600

    Deleted i18n export

--- src/services/i18n/index.ts
+++ src/services/i18n/index.ts
@@ -1,6 +1,5 @@
 import { withTranslation, WithTranslation, useTranslation } from 'react-i18next';
 import { tKeys } from './constants';
-import { default as i18n } from './i18nContainer';
 import { default as LanguageSelector } from './view/LanguageSelector/LanguageSelector';
 
-export { tKeys, withTranslation, WithTranslation, useTranslation, i18n, LanguageSelector };
+export { tKeys, withTranslation, WithTranslation, useTranslation, LanguageSelector };
