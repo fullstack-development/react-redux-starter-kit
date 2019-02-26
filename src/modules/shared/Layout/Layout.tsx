6c7f8ab25720226e26586432ed273f03d369d2cd
chmnkh <chmnkh@gmail.com>
2019-02-26 16:02:29 +0700

    #78 add layout header menu component

--- src/modules/shared/Layout/Layout.tsx
+++ src/modules/shared/Layout/Layout.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import block from 'bem-cn';
-import { Link, withRouter, RouteComponentProps } from 'react-router-dom';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import { bind } from 'decko';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener'; // TODO: move
 
-import { MenuIcon } from 'shared/view/elements';
 import { featureConnect } from 'core';
 import * as features from 'features';
 
+import LayoutHeaderMenu, { IMenuItem } from './LayoutHeaderMenu/LayoutHeaderMenu';
 import routes from '../../routes';
 import './Layout.scss';
-
-interface IState {
-  isMenuOpen: boolean;
-}
 
 interface IOwnProps {
   title: string;
@@ -28,29 +23,26 @@ type IProps = IOwnProps & IFeatureProps & RouteComponentProps & React.ComponentP
 const b = block('layout');
 
 class Layout extends React.Component<IProps> {
-  public state: IState = {
-    isMenuOpen: false,
-  };
+  private menuItems: IMenuItem[] = [
+    {
+      path: routes.search.users.getRoutePath(),
+      title: 'Users',
+    },
+    {
+      path: routes.search.repositories.getRoutePath(),
+      title: 'Repositories',
+    },
+  ];
 
   public render() {
-    const { isMenuOpen } = this.state;
     const { children, title, profileFeatureEntry: { containers } } = this.props;
     const { ProfilePreview } = containers;
     return (
       <div className={b()}>
         <header className={b('header')}>
           <div className={b('header-content')}>
-            <div className={b('header-left-part')}>
-              <div className={b('menu', { open: isMenuOpen })} onMouseUp={this.handleMenuMouseup}>
-                <MenuIcon />
-              </div>
-              <span className={b('header-title')}>Search for:</span>
-              <ClickAwayListener onClickAway={this.handleTabsClickAway}>
-                <div className={b('tabs')}>
-                  {this.renderTab(routes.search.users.getRoutePath(), 'Users')}
-                  {this.renderTab(routes.search.repositories.getRoutePath(), 'Repositories')}
-                </div>
-              </ClickAwayListener>
+            <div className={b('menu')}>
+              <LayoutHeaderMenu menuItems={this.menuItems} />
             </div>
             <ProfilePreview onEditClick={this.handleEditProfileClick} />
           </div>
@@ -72,31 +64,10 @@ class Layout extends React.Component<IProps> {
     );
   }
 
-  private renderTab(path: string, tabTitle: string) {
-    return (
-      <Link to={path} className={b('navigation-link')}>
-        <div className={b('tab', { active: path === location.pathname })}>
-          {tabTitle}
-        </div>
-      </Link>
-    );
-  }
-
   @bind
   private handleEditProfileClick() {
     const { history } = this.props;
     history.push(routes.profile.getRoutePath());
-  }
-
-  @bind
-  private handleMenuMouseup(e: React.MouseEvent<HTMLDivElement>) {
-    e.preventDefault();
-    this.setState((prevState: IState) => ({ isMenuOpen: !prevState.isMenuOpen }));
-  }
-
-  @bind
-  private handleTabsClickAway() {
-    this.setState({ isMenuOpen: false });
   }
 }
 
---
+++ src/modules/shared/Layout/LayoutHeaderMenu/LayoutHeaderMenu.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import block from 'bem-cn';
+import { bind } from 'decko';
+import { Link } from 'react-router-dom';
+
+import { MenuIcon } from 'shared/view/elements';
+import { ClickAwayListener } from 'shared/view/components';
+
+import './LayoutHeaderMenu.scss';
+
+interface IMenuItem {
+  path: string;
+  title: string;
+}
+
+interface IState {
+  isMenuOpen: boolean;
+}
+
+interface IProps {
+  menuItems: IMenuItem[];
+}
+
+const b = block('layout-header-menu');
+
+class LayoutHeaderMenu extends React.PureComponent<IProps, IState> {
+  public state: IState = {
+    isMenuOpen: false,
+  };
+
+  public render() {
+    const { isMenuOpen } = this.state;
+    const { menuItems } = this.props;
+    return (
+      <div className={b()}>
+        <div
+          className={b('menu-icon', { open: isMenuOpen })}
+          onClick={this.handleMenuClick}
+          onTouchEnd={this.handleMenuTouchEnd}
+        >
+          <MenuIcon />
+        </div>
+        <span className={b('title')}>Search for:</span>
+        <ClickAwayListener
+          onClickAway={this.handleMenuItemsClickAway}
+          mouseEvent="onClick"
+          touchEvent="onTouchEnd"
+        >
+          <div className={b('menu-items')}>
+            {menuItems.map(this.renderMenuItem)}
+          </div>
+        </ClickAwayListener>
+      </div>
+    );
+  }
+
+  private renderMenuItem({ path, title }: IMenuItem, i: number) {
+    return (
+      <Link to={path} className={b('menu-item', { active: path === location.pathname })} key={i}>
+        {title}
+      </Link>
+    );
+  }
+
+  private toggleMenu(e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>) {
+    e.preventDefault();
+    this.setState((prevState: IState) => ({ isMenuOpen: !prevState.isMenuOpen }));
+  }
+
+  @bind
+  private handleMenuClick(e: React.MouseEvent<HTMLDivElement>) {
+    this.toggleMenu(e);
+  }
+
+  @bind
+  private handleMenuTouchEnd(e: React.TouchEvent<HTMLDivElement>) {
+    this.toggleMenu(e);
+  }
+
+  @bind
+  private handleMenuItemsClickAway() {
+    this.setState({ isMenuOpen: false });
+  }
+}
+
+export { IMenuItem };
+export default LayoutHeaderMenu;
