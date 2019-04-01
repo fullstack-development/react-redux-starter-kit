1355cd0a79df5add82f655d0a52faf04680f9162
chmnkh <chmnkh@gmail.com>
2019-04-01 15:35:51 +0700

    Demo app tests: change rule helper test

--- src/shared/helpers/style/__tests__/rule.test.ts
+++ src/shared/helpers/style/__tests__/rule.test.ts
@@ -1,8 +1,13 @@
 import { rule } from '../';
 
 describe('(shared/helpers/style) rule', () => {
-  it('should not change props', () => {
-    const props = {};
-    expect(rule(props)).toBe(props);
+  it('should only add TypeScript type and not change input styles', () => {
+    const JSSStyles = {
+      top: 0,
+      left: 0,
+      width: 322,
+    };
+    const JSSStylesWithType = rule(JSSStyles);
+    expect(JSSStyles).toEqual(JSSStylesWithType);
   });
 });
