03b9d5d87c9e722c2646cb928c4e4f2c1199e194
chmnkh <chmnkh@gmail.com>
2019-04-01 15:58:25 +0700

    Demo app tests: areComponentsEqual rewrite

--- src/shared/helpers/react/__tests__/areComponentsEqual.test.ts
+++ src/shared/helpers/react/__tests__/areComponentsEqual.test.ts
@@ -2,14 +2,17 @@ import { makeMockComponent } from 'shared/mocks';
 import { areComponentsEqual } from '..';
 
 describe('areComponentsEqual', () => {
-  const FirstComponent = makeMockComponent('FirstComponent');
-  const SecondComponent = makeMockComponent('SecondComponent');
-
-  it('should return true if components are equal', () => {
+  it('should return true if components share the same name', () => {
+    const componentName = 'FirstComponent';
+    const FirstComponent = makeMockComponent(componentName);
+    const FirstComponentCopy = makeMockComponent(componentName);
     expect(areComponentsEqual(FirstComponent, FirstComponent)).toBe(true);
+    expect(areComponentsEqual(FirstComponent, FirstComponentCopy)).toBe(true);
   });
 
-  it('should false if components are not equal', () => {
+  it('should return false if components do not share the same name', () => {
+    const FirstComponent = makeMockComponent('FirstComponent');
+    const SecondComponent = makeMockComponent('SecondComponent');
     expect(areComponentsEqual(FirstComponent, SecondComponent)).toBe(false);
   });
 });
