ef7b97d524988103670914d2e0773f259b5eed63
chmnkh <chmnkh@gmail.com>
2019-04-03 11:11:37 +0700

    Demo app lang presets: add lang icons

--- src/shared/view/elements/Icons/index.ts
+++ src/shared/view/elements/Icons/index.ts
@@ -1,2 +1,3 @@
 export { default as StarIcon } from '@material-ui/icons/Star';
 export { default as MenuIcon } from '@material-ui/icons/Menu';
+export * from './languages';
---
+++ src/shared/view/elements/Icons/languages/index.ts
@@ -0,0 +1,7 @@
+export { default as CppIcon } from './CppIcon';
+export { default as CsIcon } from './CsIcon';
+export { default as JSIcon } from './JSIcon';
+export { default as TSIcon } from './TSIcon';
+export { default as RubyIcon } from './RubyIcon';
+export { default as PythonIcon } from './PythonIcon';
+export { default as JavaIcon } from './JavaIcon';
