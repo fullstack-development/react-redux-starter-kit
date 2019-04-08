4c0e46a9901e6a062d778e4fd3afe348c309611c
clicktronix <s28.morgan@gmail.com>
2019-04-08 17:38:44 +0300

    Tests fixed with new mui select, jest downgraded

--- src/services/i18n/view/LanguageSelector/__tests__/index.test.ts
+++ src/services/i18n/view/LanguageSelector/__tests__/index.test.ts
@@ -1,5 +1,6 @@
 import { makeShallowRenderer, getMockedLocaleProps } from 'shared/helpers';
 import { WithTranslation } from 'services/i18n';
+import { Select } from 'shared/view/elements';
 
 import { LanguageSelector } from '../LanguageSelector';
 
@@ -12,7 +13,7 @@ const getComponent = makeShallowRenderer(LanguageSelector, props);
 describe('(services/i18n) LanguageSelector component', () => {
   it('should render select', () => {
     const component = getComponent();
-    expect(component.find('select').length).toEqual(1);
+    expect(component.find(Select).length).toEqual(1);
   });
 
   it('should call changeLanguage', () => {
@@ -26,7 +27,7 @@ describe('(services/i18n) LanguageSelector component', () => {
     };
     const component = getComponent(newProps);
 
-    component.find('select').simulate('change', { target: { value: 'ru-RU' } });
+    component.find(Select).simulate('change', { target: { value: 'ru-RU' } });
 
     expect(changeLanguage).toBeCalledTimes(1);
     expect(changeLanguage).toBeCalledWith('ru-RU');
@@ -35,24 +36,18 @@ describe('(services/i18n) LanguageSelector component', () => {
   it('should render select with en-US default language', () => {
     const component = getComponent();
 
-    expect(component.find('select').props().value).toEqual('en-US');
+    expect(component.find(Select).props().value).toEqual('en-US');
   });
 
-  it('should render right options value', () => {
+  it('should get correct options', () => {
+    const expectedOptions = [{
+      label: 'English', value: 'en-US',
+    }, {
+      label: 'Русский', value: 'ru-RU',
+    }];
     const component = getComponent();
-    const options = component.find('option');
+    const select = component.find(Select);
 
-    expect(options.length).toEqual(2);
-    expect(options.at(0).props().value).toEqual('en-US');
-    expect(options.at(1).props().value).toEqual('ru-RU');
-  });
-
-  it('should render right labels', () => {
-    const component = getComponent();
-    const options = component.find('option');
-
-    expect(options.length).toEqual(2);
-    expect(options.at(0).props().children).toEqual('English');
-    expect(options.at(1).props().children).toEqual('Русский');
+    expect(select.props().options).toEqual(expectedOptions);
   });
 });
