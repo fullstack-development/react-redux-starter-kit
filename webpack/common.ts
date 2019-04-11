12e6198ac9f2d1b6aec1ce3c1ab0f9dbe46b2727
chmnkh <chmnkh@gmail.com>
2019-04-11 12:10:53 +0700

    Add pretty-quick and change staged-linting

--- webpack/common.ts
+++ webpack/common.ts
@@ -13,7 +13,6 @@ import FileManagerWebpackPlugin from 'filemanager-webpack-plugin';
 import postcssReporter from 'postcss-reporter';
 import postcssSCSS from 'postcss-scss';
 import autoprefixer from 'autoprefixer';
-import stylelint from 'stylelint';
 import doiuse from 'doiuse';
 
 import getEnvParams from '../src/core/getEnvParams';
@@ -28,104 +27,107 @@ const threadLoader: webpack.Loader[] = (() => {
       workers: require('os').cpus().length - 1,
       poolTimeout: withHot ? Infinity : 2000,
     };
-    isWatchMode && threadLoaderLib.warmup(workerPool, [
-      'babel-loader',
-      'ts-loader',
-      'postcss-loader',
-      'sass-loader',
-    ]);
+    isWatchMode && threadLoaderLib.warmup(workerPool, ['babel-loader', 'ts-loader', 'postcss-loader', 'sass-loader']);
     return [{ loader: 'thread-loader', options: workerPool }];
   }
   return [];
 })();
 
-export const getCommonPlugins: (type: BuildType) => webpack.Plugin[] = (type) => [
-  new CleanWebpackPlugin(['build', 'static'], { root: path.resolve(__dirname, '..') }),
-  new MiniCssExtractPlugin({
-    filename: `css/[name].[${chunkHash}].css`,
-    chunkFilename: `css/[id].[${chunkHash}].css`,
-  }),
-  new HtmlWebpackPlugin({
-    filename: 'index.html',
-    template: 'assets/index.html',
-    chunksSortMode: sortChunks,
-  }),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV_MODE': JSON.stringify(process.env.NODE_ENV_MODE),
-    '__HOST__': JSON.stringify('http://localhost:3000'),
-    '__LANG__': JSON.stringify(process.env.LANG || 'en'),
-    '__CLIENT__': true,
-    '__SERVER__': false,
-  }),
-  new CircularDependencyPlugin({
-    exclude: /node_modules/,
-    failOnError: true,
-    cwd: process.cwd(),
-  }),
-  new FaviconsWebpackPlugin(path.resolve(__dirname, '..', 'src', 'assets', 'favicon.png')),
-]
-  .concat(type !== 'server' ? (
-    new ForkTsCheckerWebpackPlugin({
-      checkSyntacticErrors: true,
-      async: false,
-      tsconfig: path.resolve('./tsconfig.json'),
-    })) : [])
-  .concat(withAnalyze ? (
-    new BundleAnalyzerPlugin()
-  ) : [])
-  .concat(withHot && type !== 'prod' ? (
-    new webpack.HotModuleReplacementPlugin()
-  ) : [])
-  .concat(forGHPages ? (
+export const getCommonPlugins: (type: BuildType) => webpack.Plugin[] = type =>
+  [
+    new CleanWebpackPlugin(['build', 'static'], { root: path.resolve(__dirname, '..') }),
+    new MiniCssExtractPlugin({
+      filename: `css/[name].[${chunkHash}].css`,
+      chunkFilename: `css/[id].[${chunkHash}].css`,
+    }),
     new HtmlWebpackPlugin({
-      filename: '404.html',
+      filename: 'index.html',
       template: 'assets/index.html',
       chunksSortMode: sortChunks,
-    })
-  ) : [])
-  .concat(forGHPages ? new FileManagerWebpackPlugin({
-    onEnd: {
-      copy: [
-        {
-          source: `src/assets/ghPages/**`,
-          destination: `build`,
-        },
-      ],
-    },
-  }) : []);
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV_MODE': JSON.stringify(process.env.NODE_ENV_MODE),
+      __HOST__: JSON.stringify('http://localhost:3000'),
+      __LANG__: JSON.stringify(process.env.LANG || 'en'),
+      __CLIENT__: true,
+      __SERVER__: false,
+    }),
+    new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true,
+      cwd: process.cwd(),
+    }),
+    new FaviconsWebpackPlugin(path.resolve(__dirname, '..', 'src', 'assets', 'favicon.png')),
+  ]
+    .concat(
+      type !== 'server'
+        ? new ForkTsCheckerWebpackPlugin({
+            checkSyntacticErrors: true,
+            async: false,
+            tsconfig: path.resolve('./tsconfig.json'),
+          })
+        : [],
+    )
+    .concat(withAnalyze ? new BundleAnalyzerPlugin() : [])
+    .concat(withHot && type !== 'prod' ? new webpack.HotModuleReplacementPlugin() : [])
+    .concat(
+      forGHPages
+        ? new HtmlWebpackPlugin({
+            filename: '404.html',
+            template: 'assets/index.html',
+            chunksSortMode: sortChunks,
+          })
+        : [],
+    )
+    .concat(
+      forGHPages
+        ? new FileManagerWebpackPlugin({
+            onEnd: {
+              copy: [
+                {
+                  source: `src/assets/ghPages/**`,
+                  destination: `build`,
+                },
+              ],
+            },
+          })
+        : [],
+    );
 
 function sortChunks(a: webpack.compilation.Chunk, b: webpack.compilation.Chunk) {
   const order = ['app', 'vendors', 'runtime'];
-  return order.findIndex(
-    // webpack typings for Chunk are not correct wait for type updates for webpack.compilation.Chunk
-    item => (b as any).names[0].includes(item)) - order.findIndex(item => (a as any).names[0].includes(item),
-    );
+  return (
+    order.findIndex(
+      // webpack typings for Chunk are not correct wait for type updates for webpack.compilation.Chunk
+      item => (b as any).names[0].includes(item),
+    ) - order.findIndex(item => (a as any).names[0].includes(item))
+  );
 }
 
-export const getCommonRules: (type: BuildType) => webpack.Rule[] = (type) => [
+export const getCommonRules: (type: BuildType) => webpack.Rule[] = type => [
   {
     test: /\.tsx?$/,
-    use:
-      threadLoader
-        .concat(withHot && type === 'dev' ? {
-          loader: 'babel-loader',
-          options: {
-            babelrc: false,
-            cacheDirectory: true,
-            plugins: [
-              'react-hot-loader/babel',
-              'syntax-dynamic-import',
-            ],
-          },
-        } : [])
-        .concat({
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-            happyPackMode: true,
-            logLevel: 'error',
-          },
-        }),
+    use: threadLoader
+      .concat(
+        withHot && type === 'dev'
+          ? {
+              loader: 'babel-loader',
+              options: {
+                babelrc: false,
+                cacheDirectory: true,
+                plugins: ['react-hot-loader/babel', 'syntax-dynamic-import'],
+              },
+            }
+          : [],
+      )
+      .concat({
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+          happyPackMode: true,
+          logLevel: 'error',
+        },
+      }),
   },
   {
     test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9]+)?$/,
@@ -186,7 +188,6 @@ const commonScssLoaders: webpack.Loader[] = [
       syntax: postcssSCSS,
       plugins: () => {
         return [
-          stylelint(),
           doiuse({
             // https://github.com/browserslist/browserslist
             // to view resulting browsers list, use the command in terminal `npx browserslist "defaults, not ie > 0"`
