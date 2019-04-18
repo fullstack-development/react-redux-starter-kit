ae35a6b80970172e76acc8ef85ba289a21f50c69
clicktronix <s28.morgan@gmail.com>
2019-04-18 14:54:24 +0300

    memoizeByProps implementation updated

--- src/shared/helpers/decorators.ts
+++ src/shared/helpers/decorators.ts
@@ -1,5 +1,3 @@
-import * as R from 'ramda';
-
 type Transmitter = (deps: any, args?: any) => any[];
 
 /**
@@ -7,7 +5,7 @@ type Transmitter = (deps: any, args?: any) => any[];
  * and returns an array of them. Also bind memoized function.
  */
 export const memoizeByProps = (transmitter: Transmitter) =>
-  (_target: any, key: any, { value: oldValue, get: getter }: PropertyDescriptor) => {
+  (_target: any, _key: any, { value: oldValue, get: getter }: PropertyDescriptor) => {
     let cache: any;
     let cachedDeps: any[] = [];
 
@@ -15,26 +13,20 @@ export const memoizeByProps = (transmitter: Transmitter) =>
       configurable: true,
       get() {
         const fn: undefined | false | ((...args: any[]) => any) = (
-          oldValue && typeof oldValue === 'function' && oldValue.bind(this) ||
-          getter && typeof getter === 'function' && getter.call(this)
+          typeof oldValue === 'function' && oldValue.bind(this) ||
+          typeof getter === 'function' && getter.call(this)
         );
         if (!fn) {
           throw new Error('Memoization can only be made with function');
         }
-        const value = (...args: any[]) => {
+        return (...args: any[]) => {
           const deps = transmitter(this.props, ...args);
-          if (!R.equals(deps, cachedDeps)) {
+          if (cachedDeps.every((x, i) => x === deps[i])) {
             cache = fn(...args);
             cachedDeps = deps;
           }
           return cache;
         };
-        Object.defineProperty(this, key, {
-          value,
-          configurable: true,
-          writable: true,
-        });
-        return value;
       },
     };
   };
