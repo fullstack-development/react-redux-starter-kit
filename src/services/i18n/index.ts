986de5bb55adbb83da2bee1c2216a117d52b52ae
clicktronix <s28.morgan@gmail.com>
2019-04-19 12:41:22 +0300

    useTranslation hook types are redeclared

--- src/services/i18n/index.ts
+++ src/services/i18n/index.ts
@@ -1,8 +1,11 @@
 // tslint:disable-next-line: import-blacklist
-import { withTranslation, useTranslation } from 'react-i18next';
+import { withTranslation, useTranslation as translationHook } from 'react-i18next';
 import { tKeys } from './constants';
 import { default as LanguageSelector } from './view/LanguageSelector/LanguageSelector';
+import { UseTranslation } from './namespace';
+
+const useTranslation = translationHook as UseTranslation;
 
 export { ITranslateKey, IConcatKey } from './helpers/buildTranslationKeys';
-export { TranslateFunction, ITranslateObject, ITranslationProps } from './namespace';
+export { TranslateFunction, ITranslateObject, ITranslationProps, UseTranslation } from './namespace';
 export { tKeys, withTranslation, useTranslation, LanguageSelector };
--- src/services/i18n/namespace.ts
+++ src/services/i18n/namespace.ts
@@ -1,5 +1,6 @@
-// tslint:disable-next-line: import-blacklist
+// tslint:disable: import-blacklist
 import i18next from 'i18next';
+import reactI18Next from 'react-i18next';
 import { ITranslateKey } from './helpers/buildTranslationKeys';
 
 export type TranslateFunction = <
@@ -19,6 +20,17 @@ export interface ITranslationProps extends IWithTFunction {
   i18n: i18next.i18n;
   tReady: boolean;
 }
+
+export interface IUseTranslationRes {
+  t: TranslateFunction;
+  i18n: i18next.i18n;
+  ready: boolean;
+}
+
+export type UseTranslation = (
+  ns?: reactI18Next.Namespace,
+  options?: reactI18Next.UseTranslationOptions,
+) => IUseTranslationRes;
 
 export interface ITranslateObject {
   key: string;
