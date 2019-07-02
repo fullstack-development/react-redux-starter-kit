11a47a554c928734c91e3bfae514d519adb6df4f
Zarwlar <barbuhs2@gmail.com>
2019-07-02 14:26:25 +0700

    Remove export

--- src/shared/helpers/react/index.ts
+++ src/shared/helpers/react/index.ts
@@ -1,4 +1,3 @@
 export { default as withProps } from './withProps';
-export { default as getFieldWithComponent } from './getFieldWithComponent';
 export { default as withComponent } from './withComponent';
 export { default as areComponentsEqual } from './areComponentsEqual';
