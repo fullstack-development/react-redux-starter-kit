d9cbc2b6d47b0fdac819385c9bd88415ea83e9d6
megakaban <nakedant19@gmail.com>
2019-09-25 15:06:58 +0700

    Test push

--- src/features/profile/redux/reducers/users.ts
+++ src/features/profile/redux/reducers/users.ts
@@ -1,2 +1,13 @@
 import * as NS from '../../namespace';
 import initial from '../initial';
+
+function usersReducer(state: NS.IReduxState, { type, payload }: any) {
+  switch (type) {
+    case '':
+
+    default:
+      return state;
+  }
+}
+
+export default usersReducer;
